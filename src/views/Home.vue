468eec8d75d80f73f4ec7169be243163ea91ca78
Monika Swidzinska <mswidzinska87@gmail.com>
2020-12-22 15:05:32 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
-  <b-container class="p-3 bg-secondary home-view rounded">
-    <b-row>
-      <div id="blue-ball" class="bg-primary rounded-circle m-4" />
-      <div class="tiny-balls bg-danger rounded-circle mr-1 mt-4" />
-      <div class="tiny-balls bg-warning rounded-circle mr-1 mt-4" />
-      <div class="tiny-balls bg-success rounded-circle mr-1 mt-4" />
-    </b-row>
-    <b-row>
-    </b-row>
-    <b-row>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container class="p-3 bg-secondary home-view rounded">
+      <b-row>
+        <div id="blue-ball" class="bg-primary rounded-circle m-4" />
+        <div class="tiny-balls bg-danger rounded-circle mr-1 mt-4" />
+        <div class="tiny-balls bg-warning rounded-circle mr-1 mt-4" />
+        <div class="tiny-balls bg-success rounded-circle mr-1 mt-4" />
+      </b-row>
+      <b-row> </b-row>
+      <b-row> </b-row>
+    </b-container>
+    <div>
+      <p>Hello {{ name }}</p>
+      <h1 v-if="red">Mike have a red ball</h1>
+      <h1 v-else-if="green">Monika have a green ball</h1>
+      <h1 v-else-if="yellow">Caren have a yellow ball</h1>
+      <h1 v-else>Phil have no ball</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,6 +25,10 @@ export default {
   name: "Home",
   data() {
     return {
+      name: "Monika",
+      red: false,
+      green: true,
+      yellow: false,
     };
   },
 };
